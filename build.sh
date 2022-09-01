3a289634419176906f074f859fb61e6e8f208eba
GrigoriyAzatyan <gregory78@yandex.ru>
2022-09-01 22:21:45 +0500

    ---

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo GIT_BRANCH: $GIT_BRANCH
-TAG=${GIT_BRANCH:13}
+TAG=${GIT_BRANCH:10}
+echo TAG: $TAG
 docker build -t gregory78/static-page:$TAG .
 cat /jenkins/docker.pwd | docker login --username gregory78 --password-stdin
 docker push gregory78/static-page:$TAG
