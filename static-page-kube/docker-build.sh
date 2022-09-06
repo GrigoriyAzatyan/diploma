b9e443440106ab072670c3e7bc35a0961e4f0f91
GrigoriyAzatyan <gregory78@yandex.ru>
2022-09-06 11:34:20 +0500

    06.09

--- static-page-kube/docker-build.sh
+++ static-page-kube/docker-build.sh
@@ -3,8 +3,5 @@
 echo GIT_BRANCH: $GIT_BRANCH
 export TAG=${GIT_BRANCH:10}
 echo TAG: $TAG
-docker build -t gregory78/static-page:$TAG .
-cat /jenkins/docker.pwd | docker login --username gregory78 --password-stdin
-docker push gregory78/static-page:$TAG
-yes | docker image prune
-kubectl apply -f static-page.yaml
+ls -l .
+cat ./static-page.yaml
